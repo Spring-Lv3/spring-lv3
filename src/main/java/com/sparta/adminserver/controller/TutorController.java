cdde534c4c9296d499552d1ad24339fbc86cb294
Gary9409 <gary9409@gmail.com>
2023-11-14 20:42:35 +0900

    TutorController 작성

--- src/main/java/com/sparta/adminserver/controller/TutorController.java
+++ src/main/java/com/sparta/adminserver/controller/TutorController.java
@@ -4,6 +4,7 @@ import com.sparta.adminserver.dto.TutorRequestDto;
 import com.sparta.adminserver.dto.TutorResponseDto;
 import com.sparta.adminserver.service.TutorService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToUrl;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,8 +13,18 @@ import org.springframework.web.bind.annotation.*;
 public class TutorController {
     private final TutorService tutorService;
 
-    @PostMapping("/")
+    @PostMapping("")
     public TutorResponseDto registeredTutor(@RequestBody TutorRequestDto requestDto) {
         return tutorService.registeredTutor(requestDto);
+    }
+
+    @PutMapping("/{tutor_id}")
+    public TutorResponseDto modifyTutor(@PathVariable Long tutor_id, @RequestBody TutorRequestDto requestDto) {
+        return tutorService.modifyTutor(tutor_id, requestDto);
+    }
+
+    @GetMapping("/{tutor_id}")
+    public TutorResponseDto findTutor(@PathVariable Long tutor_id) {
+        return tutorService.findTutor(tutor_id);
     }
 }
