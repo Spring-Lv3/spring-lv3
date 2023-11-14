cdda33611adeba5c6c3fcf2a297c5c8f2287c65a
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-14 22:01:09 +0900

    회원,로그인 관련 api 빼고 Jwt 같이 요청해야 하도록 수정

--- src/main/java/com/sparta/adminserver/controller/TutorController.java
+++ src/main/java/com/sparta/adminserver/controller/TutorController.java
@@ -1,8 +1,11 @@
 package com.sparta.adminserver.controller;
 
+import com.sparta.adminserver.config.swagger.ApiDocument;
 import com.sparta.adminserver.dto.TutorRequestDto;
 import com.sparta.adminserver.dto.TutorResponseDto;
+import com.sparta.adminserver.jwt.JwtRequired;
 import com.sparta.adminserver.service.TutorService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,16 +15,25 @@ import org.springframework.web.bind.annotation.*;
 public class TutorController {
     private final TutorService tutorService;
 
+    @JwtRequired
+    @ApiDocument
+    @Operation(summary = "test", description = "test")
     @PostMapping("")
     public TutorResponseDto registerTutor(@RequestBody TutorRequestDto requestDto) {
         return tutorService.registerTutor(requestDto);
     }
 
+    @JwtRequired
+    @ApiDocument
+    @Operation(summary = "test", description = "test")
     @PutMapping("/{tutor_id}")
     public TutorResponseDto modifyTutor(@PathVariable Long tutor_id, @RequestBody TutorRequestDto requestDto) {
         return tutorService.modifyTutor(tutor_id, requestDto);
     }
 
+    @JwtRequired
+    @ApiDocument
+    @Operation(summary = "test", description = "test")
     @GetMapping("/{tutor_id}")
     public TutorResponseDto findTutor(@PathVariable Long tutor_id) {
         return tutorService.findTutor(tutor_id);
