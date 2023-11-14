4c988adb6c5cc51716d7bd4517a24925258fb517
Gary9409 <gary9409@gmail.com>
2023-11-14 19:18:02 +0900

    lecture request dto

--- src/main/java/com/sparta/adminserver/dto/LectureRequestDto.java
+++ src/main/java/com/sparta/adminserver/dto/LectureRequestDto.java
@@ -1,8 +1,14 @@
 package com.sparta.adminserver.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class LectureRequestDto {
-    private name;
+    private String name;
+    private Long price;
+    private String comment;
+    private String category;
+    private Long tutorId;
 }
