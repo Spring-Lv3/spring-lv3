dcae2f062dee7c55dcda47f598d758d4ffb5de6b
Gary9409 <gary9409@gmail.com>
2023-11-14 12:21:18 +0900

    entity/Tutor 생성

--- src/main/java/com/sparta/adminserver/entity/Tutor.java
+++ src/main/java/com/sparta/adminserver/entity/Tutor.java
@@ -1,8 +1,41 @@
 package com.sparta.adminserver.entity;
 
-import jakarta.persistence.Entity;
+import com.sparta.adminserver.dto.LectureRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "tutor")
 public class Tutor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tutor_id;
 
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long career;
+
+    @Column(nullable = false)
+    private String company;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(length = 500)
+    private String comment;
+
+    public Tutor(LectureRequestDto requestDto) {
+        this.name = requestDto.getName();
+        this.career = requestDto.getCareer();
+        this.company = requestDto.getCompany();
+        this.phoneNumber = requestDto.getPhoneNumber();
+        this.comment = requestDto.getComment();
+    }
 }
